e0aec84a8164f10170134614a10d14b876b179cc
marco-gulino <marcogulino98@gmail.com>
2025-01-16 16:06:36 +0100

    [feat/js] create an array of objects

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//Snack 1
+//Creo un array di oggetti
+const racingBike = [
+  {
+    name: "nome1",
+    weight: 22,
+  },
+
+  {
+    name: "nome2",
+    weight: 10,
+  },
+
+  {
+    name: "nome3",
+    weight: 30,
+  },
+
+  {
+    name: "nome4",
+    weight: 44.2,
+  },
+
+  {
+    name: "nome5",
+    weight: 9.2,
+  },
+
+  {
+    name: "nome6",
+    weight: 100,
+  },
+];
